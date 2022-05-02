2ded0a29052d3ce2b4028028aff763a5ecc91875
Sergey Petrinich <petrinichshadow@gmail.com>
2022-05-02 16:50:34 +0300

    footer v1

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,11 @@
 import logo from "./../assets/img/logo.svg";
-import logoMin from "./../assets/img/logo-min.svg";
+import logoMin from "./../assets/img/logo_min.svg";
+import logoFooter from "./../assets/img/logo_footer.svg";
 
 const defaultImages = {
     logo,
     logoMin,
+    logoFooter,
 };
 
 export default { defaultImages };
